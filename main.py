a91a6be12cdfb8ee49fa32081234fa511faaaa68
SoInstant <44108612+SoInstant@users.noreply.github.com>
2020-12-22 22:20:39 +0800

    Bot

---
+++ main.py
@@ -0,0 +1,150 @@
+import os
+from datetime import datetime
+from time import sleep
+
+from discord.ext import commands, timers
+import discord
+from dotenv import load_dotenv
+
+import keep_alive
+import utils
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+OWNER_ID = int(os.getenv("OWNER_ID"))
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+
+bot = commands.Bot(command_prefix="!")
+bot.timer_manager = timers.TimerManager(bot)
+
+
+@bot.check
+async def block_dms(ctx):
+    return ctx.guild != None
+
+
+@bot.check
+async def block_all(ctx):
+    return ctx.author.id == OWNER_ID
+
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} has connected to Discord!")
+    await bot.change_presence(
+        activity=discord.Activity(
+            type=discord.ActivityType.watching, name="over reminders"
+        )
+    )
+    await bot.get_channel(CHANNEL_ID).send(content=f"<@{OWNER_ID}> Bot is now online!")
+    tasks = utils.get_reminders()
+    for task in tasks:
+        bot.timer_manager.create_timer(
+            "reminder",
+            task["time_warn"] - int(datetime.now().timestamp()),
+            args=(task["task"], utils.unix_to_timestamp(task["time_due"])),
+        )
+
+
+@bot.event
+async def on_reminder(task, time):
+    await bot.get_channel(CHANNEL_ID).send(
+        f'<@{OWNER_ID}> The task "{task}" is due at {time}!'
+    )
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CommandNotFound):
+        await ctx.channel.send(
+            content=":negative_squared_cross_mark: No such command exists!"
+        )
+
+
+@bot.command(help="Clears n messages from the channel.")
+async def clearmsg(ctx, n=None):
+    if n:
+        n = int(n)
+    counter = 0
+    async for message in ctx.channel.history(limit=n):
+        counter += 1
+        await message.delete()
+    if counter == 1:
+        await ctx.channel.send(content=":white_check_mark: 1 message has been deleted!")
+    else:
+        await ctx.channel.send(
+            content=f":white_check_mark: {counter} messages have been deleted!"
+        )
+
+
+@bot.group(name="reminders", invoke_without_command=True)
+async def reminders(ctx):
+    await ctx.channel.send(
+        content="Base reminders command. Subcommands: list, add, delete"
+    )
+
+
+@reminders.command(name="list", help="Lists all pending tasks.")
+async def list_reminders(ctx):
+    reminders = utils.get_reminders()
+    if reminders == []:
+        await ctx.channel.send(content="You have no current tasks to complete! :tada:")
+    else:
+        embed = discord.Embed(title="__Here are your pending tasks:__")
+        embed.set_author(
+            name="Sir SoInstant",
+            url="https://soinstant.ml",
+            icon_url="https://i.pinimg.com/originals/7d/41/f4/7d41f4a15bd89da6a65856e69cc6e2cc.png",
+        )
+        for i, document in enumerate(reminders):
+            embed.add_field(
+                name=f"{i+1}. **{document['task']}** (Due on {utils.unix_to_timestamp(document['time_due'])})",
+                value=f"- {document['description']}",
+                inline=False,
+            )
+        embed.set_footer(text="'You probably won't remember me' - You Meet in Paris")
+        await ctx.channel.send(embed=embed)
+
+
+@reminders.command(
+    name="add",
+    help="Adds a task. Argument must be in the format of '<name of task>' '<date due, in the format of %Y/%m/%D %H:%M>' '<date warn>' '<description of task>'",
+)
+async def add_reminder(ctx, task: str, due: str, warn: str, desc: str):
+    utils.add_reminder(task=task, due=due, warn=warn, desc=desc)
+    await ctx.channel.send(content=":white_check_mark: Your reminder has been added!")
+    bot.timer_manager.create_timer(
+        "reminder",
+        utils.timestamp_to_unix(warn) - int(datetime.now().timestamp()),
+        args=(task, due),
+    )
+
+
+@reminders.command(
+    name="delete",
+    help="Deletes a task. The argument, n, is the index of the task from !reminders list",
+)
+async def delete_reminder(ctx, n: int):
+    if utils.delete_reminder(n):
+        await ctx.channel.send(
+            content=f":white_check_mark: Reminder no. {n} has been deleted!"
+        )
+    else:
+        await ctx.channel.send(
+            content=f":negative_squared_cross_mark: Index out of bounds!"
+        )
+
+
+@reminders.command(name="cleardone", help="Clears all completed tasks from the DB.")
+async def clear(ctx):
+    if utils.clear_finished():
+        await ctx.channel.send(
+            content=":white_check_mark: Successfully cleared completed tasks!"
+        )
+    else:
+        await ctx.channel.send(content="Something went wrong!")
+
+
+if __name__ == "__main__":
+    keep_alive.keep_alive()
+    bot.run(TOKEN)
---
+++ utils.py
@@ -0,0 +1,126 @@
+import os
+from datetime import datetime, timezone, timedelta
+
+import pymongo
+from dotenv import load_dotenv
+
+load_dotenv()
+USERNAME = os.getenv("DB_USERNAME")
+PASSWORD = os.getenv("DB_PASSWORD")
+
+client = pymongo.MongoClient(
+    f"mongodb+srv://{USERNAME}:{PASSWORD}@cluster0.7ivzo.mongodb.net/<dbname>?authSource=admin&replicaSet=atlas-9wc5l5-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true"
+)
+
+db = client["app"]
+
+
+def get_reminders():
+    """Returns a list of pending tasks
+
+    Params:
+        None
+
+    Returns:
+        A list of documents containing information about the tasks
+    """
+    col = db["reminders"]
+    results = [
+        document
+        for document in col.find({"completed": False}, {"completed": 0}).sort(
+            "time_due", 1
+        )
+    ]
+    return results
+
+
+def add_reminder(task, due, warn, desc):
+    """Adds a task
+
+    Params:
+        task (str): The name of the task
+        due (str): The date the task is due, in the format of '%Y/%m/%d %H:%M'
+        warn (str): The date to warn about the task, in the format of '%Y/%m/%d %H:%M'
+        desc (str): Any additional information pertaining to the task
+
+    Returns:
+        A boolean which indicates if the insert operation was successful.
+    """
+    col = db["reminders"]
+    desc = desc.replace("\n", "\n- ")
+    op = col.insert_one(
+        {
+            "task": task,
+            "time_due": timestamp_to_unix(due),
+            "time_warn": timestamp_to_unix(warn),
+            "description": desc,
+            "completed": False,
+        }
+    )
+    return op.acknowledged == True
+
+
+def delete_reminder(index):
+    """Marks a task as complete
+
+    Params:
+        index (int): Index of the task to be marked as complete
+
+    Returns:
+        A boolean which indicates if the update operation was successful.
+    """
+    reminders = get_reminders()
+    if index < 0 or index > len(reminders):
+        return False
+
+    col = db["reminders"]
+    op = col.update_one(
+        {"_id": reminders[index - 1]["_id"]}, {"$set": {"completed": True}}
+    )
+    return op.acknowledged == True
+
+
+def clear_finished():
+    """Deletes all completed tasks from the MongoDB
+
+    Params:
+        None
+
+    Return:
+        A boolean indicating if the delete operation was successful.
+    """
+    col = db["reminders"]
+    op = col.delete_many({"completed": True})
+    return op.acknowledged == True
+
+
+def unix_to_timestamp(unix_time):
+    """Converts Unix time to a timestamp
+
+    Params:
+        unix_time (int): The Unix time to be converted
+
+    Returns:
+        A string representing the converted timestamp, in the format of '%Y/%m/%d %H:%M'
+    """
+    dtobj = datetime.fromtimestamp(unix_time, timezone.utc) + timedelta(hours=8)
+    return dtobj.isoformat(sep=" ", timespec="minutes").replace("-", "/")[:-6]
+
+
+def timestamp_to_unix(timestamp):
+    """Takes a timestamp and converts it to Unix time
+
+    Params:
+        timestamp (str): The timestamp to be converted, in the format of
+            '%Y/%m/%d %H:%M'
+
+    Returns:
+        An integer representing the Unix time
+    """
+    dt = datetime.strptime(timestamp, "%Y/%m/%d %H:%M") - timedelta(hours=8)
+    return int((dt - datetime(1970, 1, 1)).total_seconds())
+
+
+if __name__ == "__main__":
+    print(unix_to_timestamp(1609459200))
+    print(timestamp_to_unix("2021/1/1 00:00"))
